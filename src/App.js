3c24d2c6cc7a3f1262ddb16455bd579c107d73b0
nishan das8 <nishandas880@gmail.com>
2022-10-02 00:46:39 +0600

    all is ok

--- src/App.js
+++ src/App.js
@@ -2,19 +2,91 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import React, { Fragment, useState, useCallback, useMemo } from 'react'
+import { Calendar, dateFnsLocalizer , Views} from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2015, 3, 17, 19, 30, 0),
+        end: new Date(2015, 3, 18, 2, 0, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2015, 3, 17, 19, 30, 0),
+        end: new Date(2015, 3, 18, 2, 0, 0),
+    },
+    {
+        title: "Conference",
+        start: new Date(2015, 3, 17, 19, 30, 0),
+        end: new Date(2015, 3, 18, 2, 0, 0),
+    },
+];
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
+    const handleSelectSlot = useCallback(
+      ({ start, end }) => {
+        const title = window.prompt('New Event name')
+        if (title) {
+          setAllEvents((prev) => [...prev, { start, end, title }])
+        }
+      },
+      [setAllEvents]
+    )
+  
+    const handleSelectEvent = useCallback(
+      (event) => window.alert(event.title),
+      []
+    )
+  
+    const { defaultDate, scrollToTime } = useMemo(
+      () => ({
+        defaultDate: new Date(2015, 3, 12),
+        scrollToTime: new Date(1970, 1, 1, 6),
+      }),
+      []
+    )
+
+    return (
+        <div className="App">
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} defaultView={Views.WEEK} startAccessor="start" onSelectEvent={handleSelectEvent}
+          onSelectSlot={handleSelectSlot}
+          selectable
+          scrollToTime={scrollToTime} endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
 }
 
 export default App;
